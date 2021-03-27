f667303588bd7eb07138cce675462d81a9dfa46c
Daniel Santos <neco.daniel@gmail.com>
2021-03-27 16:22:02 -0300

    inicializando e carregando repositorio projeto

---
+++ src/test/java/br/com/deliverit/instant/account/interest/calculator/InstantAccountInterestCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deliverit.instant.account.interest.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantAccountInterestCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
